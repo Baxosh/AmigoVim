fca6b5b699775d8a873521ba82d0ba10a03c6fd7
bakha_aka <bt@room.io>
2024-12-14 15:40:42 +0200

    changed colorscheme and resized db result window

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dbout", -- Match the result buffer type
+  callback = function()
+    vim.cmd("vertical resize 80") -- Set preferred width
+    vim.cmd("resize 30") -- Set preferred height
+  end,
+})
