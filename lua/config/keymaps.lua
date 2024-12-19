233b38febefc061fdaf086549f8248ec507c2764
bakha_aka <bt@room.io>
2024-12-19 21:26:28 +0200

    removed chatgpt

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.set("n", "<leader>fu", ":Telescope oldfiles<CR>", { desc = "Find Recently Used Files" })
-vim.keymap.set("n", "<leader>fh", function()
-  require("telescope.builtin").find_files({ hidden = true })
-end, { desc = "Find Hidden Files" })
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,7 +4,6 @@ return {
     opts = function(_, opts)
       local null_ls = require("null-ls")
       opts.sources = vim.list_extend(opts.sources or {}, {
-        null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
       })
     end,
