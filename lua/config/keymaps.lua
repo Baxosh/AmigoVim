252253edad86a27b2d62dddaec343d18c67df14e
bakha_aka <bt@room.io>
2024-12-25 11:14:23 +0200

    Added left and right scroll via keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,7 @@
 vim.keymap.set("n", "<leader>ba", function()
   vim.cmd("bufdo bd!")
 end, { desc = "Close All Buffers" })
+
+-- Center horizontally: scroll the current line to the center horizontally
+vim.keymap.set("n", "zh", "zL", { desc = "Scroll right to center horizontally" })
+vim.keymap.set("n", "zl", "zH", { desc = "Scroll left to center horizontally" })
